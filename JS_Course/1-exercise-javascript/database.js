ef6683d2ed899272fa15ec444a081da518764b23
bokink2099 <assyamsi30@gmail.com>
2021-02-24 11:02:35 +0700

    nambah folder js course

---
+++ JS_Course/1-exercise-javascript/database.js
@@ -0,0 +1,88 @@
+const data = [
+  {
+    json: {
+      id: "recfeVSoVuduJaQL",
+      fields: {
+        DateTimeOfClose: "2021-01-18T00:32:00.842Z",
+        SalesmanName: "Nazariy Dumanskyy",
+        Cash: 1500,
+        Name: "Hajam Ban",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acct_17YWhEAmH5VbdPbp/ch_1IAlexAmH5VbdPbp3LKqK2dp/rcpt_Imfefes6QHCwvvn247FAY"
+      },
+      createdTime: "2021-01-18T00:32:05.000Z"
+    }
+  },
+  {
+    json: {
+      id: "rec8onBfes3pOrFjDd",
+      fields: {
+        DateTimeOfClose: "2021-01-17T05:07:59.517Z",
+        SalesmanName: "Daniel Seyffert",
+        Cash: 1200,
+        Name: "Bali Kooli",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acct_1fes5VbdPbp/ch_1IATURAmH5VbdPbpVSfeJl/feCCOTIfJIYuSMfZ8DPogXZj"
+      },
+      createdTime: "2021-01-17T05:08:04.000Z"
+    }
+  },
+  {
+    json: {
+      id: "recfeCpAejs0VTmVy",
+      fields: {
+        DateTimeOfClose: "2021-01-17T01:41:09.552Z",
+        SalesmanName: "Nazariy Dumanskyy",
+        Cash: 500,
+        Name: "Elizabeth Hawthorne",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acct_feshEAmH5VbdPbp/ch_1IA3TEAmH5VbdPbpjKvxChcS/rcpt_IfepxW5AjXWtAuA2QJACiu"
+      },
+      createdTime: "2021-01-17T01:41:13.000Z"
+    }
+  },
+  {
+    json: {
+      id: "rec3ffel6q8l4LR9F",
+      fields: {
+        DateTimeOfClose: "2021-01-17T01:41:02.645Z",
+        SalesmanName: "Nazariy Dumanskyy",
+        Cash: 500,
+        Name: "Elizabeth Hawthorne",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acctfesYWhEAmH5VbdPbp/ch_1IA3X7AmH5VbdPbpsFoGealt/rcpt_Ilai7tQVifes323kovm8moYM"
+      },
+      createdTime: "2021-01-17T01:41:07.000Z"
+    }
+  },
+  {
+    json: {
+      id: "rec3f4Ml6fel4LR9QL",
+      fields: {
+        DateTimeOfClose: "2021-01-17T01:41:02.645Z",
+        SalesmanName: "Rafeh Qazi",
+        Cash: 1300,
+        Name: "Luke Feinstein",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acct_17YWfesH5VbdPbp/ch_1IA3X7AmH5VbdPbpsFoGealt/rcpt_Ilaifes8KewwIVqq7323kovm8moYM"
+      },
+      createdTime: "2021-01-17T01:41:07.000Z"
+    }
+  },
+  {
+    json: {
+      id: "recki4t0feXTN",
+      fields: {
+        DateTimeOfClose: "2021-01-17T00:21:57.108Z",
+        SalesmanName: "Nazariy Dumanskyy",
+        Cash: 5000,
+        Name: "Sammy Becker",
+        ReceiptURL:
+          "https://pay.stripe.com/receipts/acct_1fesAmH5VbdPbp/ch_1IAOVAAmH5VbdPbpMRwoQlmL/rcpt_IlwNRKYNcfeshoQQFuxpZeyka"
+      },
+      createdTime: "2021-01-17T00:21:58.000Z"
+    }
+  }
+];
+
+module.exports = data
---
+++ JS_Course/1-exercise-javascript/index.js
@@ -0,0 +1,60 @@
+const data = require('./database.js');
+
+const today = new Date();
+const seven_days_ago = new Date(today);
+seven_days_ago.setDate(seven_days_ago.getDate() - 7);
+
+const sevenDaysPretty = seven_days_ago
+	.toDateString()
+	.split(' ')
+	.splice(1, 2)
+	.join(' ');
+
+const todayPretty = today.toDateString().split(' ')[2];
+
+let salesmanStats = {};
+let textMessage = `💥 Weekly Sales (${sevenDaysPretty} 👉 ${todayPretty})
+`;
+
+const getAllSalesman = data.map(item => item.json.fields.SalesmanName);
+const uniqueSalesman = [...new Set(getAllSalesman)];
+
+for (let salesman of uniqueSalesman) {
+	const filteredSalesman = data.filter(
+		item => item.json.fields['SalesmanName'] === salesman
+	);
+
+	const totalSalesofSalesman = filteredSalesman.reduce(
+		(prev, cur) => prev + cur.json.fields.Cash,
+		0
+	);
+
+	salesmanStats = { ...salesmanStats, [salesman]: totalSalesofSalesman };
+}
+
+let leaderboard = [];
+for (let salesman in salesmanStats) {
+	leaderboard = [...leaderboard, [salesman, salesmanStats[salesman]]];
+}
+
+leaderboard.sort(function(a, b) {
+	return b[1] - a[1];
+});
+
+leaderboard[0][0] = `🏆${leaderboard[0][0]}`;
+
+let index = 0;
+
+for (let [salesPerson, revenue] of leaderboard) {
+	index += 1;
+	textMessage += `
+  ${index}\. ${salesPerson.split(' ')[0]} 💵 \$${revenue.toLocaleString('en')}
+  `;
+	salesmanStats = { ...salesmanStats, message: textMessage };
+}
+
+console.log(leaderboard);
+
+console.log([{ json: salesmanStats }]);
+
+//
